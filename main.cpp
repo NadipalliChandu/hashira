72bef0574478289da1d57749fa8d5a815bfafce2
chandu <nadipallichandu988@gmail.com>
2025-07-28 12:14:42 +0530

    asdf

--- main.cpp
+++ main.cpp
@@ -1,53 +1,91 @@
 #include <iostream>
+#include <fstream>
+#include <cmath>
+#include <map>
 #include <vector>
-using namespace std;
+#include <string>
+#include "json.hpp" 
 
-int polynomial(int x, int secret, int a1, int a2) {
-    return secret + a1 * x + a2 * x * x;
+using namespace std;
+using json = nlohmann::json;
+
+using ll = long long;
+
+
+ll baseToDecimal(const string &value, int base) {
+    ll result = 0;
+    for (char ch : value) {
+        int digit;
+        if (isdigit(ch)) digit = ch - '0';
+        else digit = tolower(ch) - 'a' + 10;
+        result = result * base + digit;
+    }
+    return result;
 }
 
-vector<pair<int, int>> generate_shares(int secret, int n, int a1, int a2) {
-    vector<pair<int, int>> shares;
 
-    for (int i = 1; i <= n; i++) {
-        int y = polynomial(i, secret, a1, a2);
-        shares.push_back({i, y}); // Store the (x, y) pair as a share
+ll lagrangeInterpolationAtZero(const vector<pair<ll, ll>> &points) {
+    ll result = 0;
+    int k = points.size();
+
+    for (int i = 0; i < k; ++i) {
+        ll xi = points[i].first;
+        ll yi = points[i].second;
+
+        double numerator = 1, denominator = 1;
+
+        for (int j = 0; j < k; ++j) {
+            if (i == j) continue;
+            numerator *= -points[j].first;
+            denominator *= (xi - points[j].first);
+        }
+
+        result += (ll)(yi * (numerator / denominator));
     }
 
-    return shares;
+    return result;
 }
 
-int reconstruct_secret(const vector<pair<int, int>>& shares) {
-    int x1 = shares[0].first, y1 = shares[0].second;
-    int x2 = shares[1].first, y2 = shares[1].second;
-    int x3 = shares[2].first, y3 = shares[2].second;
+vector<pair<ll, ll>> extractPoints(const json &data) {
+    vector<pair<ll, ll>> points;
 
-    int secret =
-        y1 * (x2 * x3) / ((x1 - x2) * (x1 - x3)) +
-        y2 * (x1 * x3) / ((x2 - x1) * (x2 - x3)) +
-        y3 * (x1 * x2) / ((x3 - x1) * (x3 - x2));
+    for (auto &el : data.items()) {
+        if (el.key() == "keys") continue;
 
-    return secret;
+        int x = stoi(el.key());
+        int base = stoi(el.value()["base"].get<string>());
+        string val = el.value()["value"];
+
+        ll y = baseToDecimal(val, base);
+        points.emplace_back(x, y);
+    }
+
+    return points;
 }
 
 int main() {
-    int secret = 123;
+    
+    vector<string> filenames = { "testcase1.json", "testcase2.json" };
 
-    int n = 5;
+    for (auto &filename : filenames) {
+        ifstream infile(filename);
+        if (!infile.is_open()) {
+            cerr << "Failed to open " << filename << endl;
+            continue;
+        }
 
-    int a1 = 2, a2 = 3;
+        json input;
+        infile >> input;
 
-    auto shares = generate_shares(secret, n, a1, a2);
+        int k = input["keys"]["k"];
+        vector<pair<ll, ll>> allPoints = extractPoints(input);
 
-    cout << "Generated Shares:\n";
-    for (auto [x, y] : shares) {
-        cout << "(" << x << ", " << y << ")\n";
+        vector<pair<ll, ll>> selectedPoints(allPoints.begin(), allPoints.begin() + k);
+
+        ll secret = lagrangeInterpolationAtZero(selectedPoints);
+
+        cout << "Secret from " << filename << ": " << secret << endl;
     }
-
-    vector<pair<int, int>> sample = { shares[0], shares[1], shares[2] };
-    int recovered = reconstruct_secret(sample);
-
-    cout << "\nRecovered Secret: " << recovered << endl;
 
     return 0;
 }
